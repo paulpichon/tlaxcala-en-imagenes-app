7636fbf97b344a00a74fb6d9d2cfaa62c9c32482
paul pichon <paul10_barca@hotmail.com>
2024-12-12 15:27:43 -0600

    feat: creacion pagina restablecer password

---
+++ src/app/cuentas/login/restablecer-password/layout.tsx
@@ -0,0 +1,31 @@
+// Iniciar sesión
+// Se usara Layouts Anidados en la estructura del proyecto
+// https://chatgpt.com/c/674542f3-d5bc-8002-847c-c817ca29ba97
+
+// Meta datos NEXTJS
+import { Metadata } from "next";
+// Fonts
+import "../../../ui/fonts";
+// metadatos
+export const metadata: Metadata = {
+    title: "Tlaxcala En Imágenes | Restablecer contraseña",
+    description: "Restablecer la contraseña, introducir nueva contraseña.",
+  };
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="es">
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <body>
+          {children}
+        </body>
+      </html>
+    );
+  }
---
+++ src/app/cuentas/login/restablecer-password/page.tsx
@@ -0,0 +1,55 @@
+// Iniciar Sesion
+// Link nextjs
+// import Link from "next/link";
+// Image next
+// import Image from "next/image";
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+// Estilos de pagina
+// import styles from "./ui/page.module.css";
+import "../../../ui/cuentas/login/restablecer-password/restablecer-password.css";
+// Header principal
+import { HeaderPrincipalTei } from "@/app/components/HeaderPrincipal";
+// Footer principal
+import FooterMain from "../../../components/FooterMain";
+// viewport
+import type { Viewport } from 'next';
+// Link nextjs
+// import Link from "next/link";
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false,
+	// Also supported by less commonly used
+	// interactiveWidget: 'resizes-visual',
+}
+
+export default function RestablecerPassword() {
+		return (
+		<div className="container-fluid container-xl">
+			<div className="row justify-content-center contenedor_principal">
+				<HeaderPrincipalTei />
+				<div className="col-sm-9 col-md-7 col-lg-6">
+					<div className="contenedor_formulario ">
+						<div className="contenedor_titulos">
+							<h3 className="subtitulo_h3">Reestablecer contraseña</h3>
+                            <p className="texto">Introduce tu nueva contraseña</p>
+						</div>
+						<div className="contenedor_formulario">
+							<form className="formulario_crear_cuenta" id="iniciar_sesion">
+								<div className="mb-4">
+									<input type="password" className="form-control inputs_crear_cuenta" id="password" placeholder="Nueva contraseña" />
+								</div>
+								<button type="submit" className="boton_registrarse">Confirmar</button>
+							  </form>
+						</div>
+					</div>
+				</div>
+				{/* Footer */}
+				<FooterMain />
+			</div>
+		</div>
+		);
+}
