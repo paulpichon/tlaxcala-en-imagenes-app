b431f05098c7618a4f30961af428a8f55dec505b
paul pichon <paul10_barca@hotmail.com>
2024-12-13 19:34:33 -0600

    feat: creacion de layout y page password-restablecido

---
+++ src/app/cuentas/confirmacion/password-restablecido/layout.tsx
@@ -0,0 +1,30 @@
+// Se usara Layouts Anidados en la estructura del proyecto
+// https://chatgpt.com/c/674542f3-d5bc-8002-847c-c817ca29ba97
+
+// Meta datos NEXTJS
+import { Metadata } from "next";
+// Fonts
+import "../../../ui/fonts";
+// metadatos
+export const metadata: Metadata = {
+		title: "Contraseña restablecida | Tlaxcala En Imágenes",
+		description: "Contraseña restablecido por el usuario.",
+};
+
+export default function RootLayout({
+		children,
+	}: Readonly<{
+		children: React.ReactNode;
+	}>) {
+		return (
+            <html lang="es">
+            <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+                    />
+                    <body>
+                            {children}
+                    </body>
+            </html>
+		);
+}
---
+++ src/app/cuentas/confirmacion/password-restablecido/page.tsx
@@ -0,0 +1,14 @@
+// bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+// Componente
+// import CorreoEnviado from "../components/CorreoEnviado";
+// estilos de la pagina, seran compartidos con la página correo-enviado
+import "../../../ui/cuentas/confirmacion/correo-enviado/correo-enviado.css";
+
+export default function CrearCuenta() {
+  return (
+		<div className="container-fluid container-xl">
+            
+        </div>
+    );
+}
