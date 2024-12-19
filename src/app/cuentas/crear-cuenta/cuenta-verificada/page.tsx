106606876d2fad09744c6c34763d76919427e6d8
paul pichon <paul10_barca@hotmail.com>
2024-12-18 19:02:59 -0600

    refactor: se agrego HeaderPrincipal

--- src/app/cuentas/crear-cuenta/cuenta-verificada/page.tsx
+++ src/app/cuentas/crear-cuenta/cuenta-verificada/page.tsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 // estilos de la pagina
 import "../../../ui/cuentas/crear-cuenta/cuenta-verificada/cuenta-verificada.css";
 // Header principal
-// import { HeaderPrincipalTei } from "@/app/components/HeaderPrincipal";
+import { HeaderPrincipalTei } from "@/app/components/HeaderPrincipal";
 // Footer principal
 import FooterPrincipal from "@/app/components/FooterMain";
 
@@ -12,12 +12,8 @@ export default function CrearCuenta() {
   return (
         <div className="container-fluid container-xl">
             <div className="row justify-content-center contenedor_principal">
-                <div className="col-md-12">
-                    <div className="contenedor_tei_index">
-                        <h1 className="titulo_principal_h1">TLAXCALA EN <span className="d-block">IMÁGENES</span></h1>
-                        <h6 className="subtitulo_h6">por <span className="span_tad d-block">T A D</span></h6>
-                    </div>
-                </div>
+                {/* Header */}
+                <HeaderPrincipalTei />
                 <div className="col-sm-9 col-md-7 col-lg-6">
                     <div className="contenedor_formulario ">
                         <div className="contenedor_titulos">
