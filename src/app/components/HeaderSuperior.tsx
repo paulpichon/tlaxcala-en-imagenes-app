a59441f0c47ba6dde9ca365e13a1be84acfc240d
paul pichon <paul10_barca@hotmail.com>
2025-01-10 13:44:37 -0600

    feat: creacion de componente header superior, que es el Header Tlaxcala en imágenes de la app

---
+++ src/app/components/HeaderSuperior.tsx
@@ -0,0 +1,11 @@
+'use client';
+export default function HeaderSuperior() {
+    return (
+        <>
+            <nav className="navbar-expand-lg menu_superior_central">
+                <h1 className="titulo_principal_h1">TLAXCALA EN <span className="display_inline_titulo">IMÁGENES</span></h1>
+                <h6 className="subtitulo_h6 por_tad">por <span className="span_tad d-block" data-bs-toggle="tooltip" data-bs-placement="right" title="Tlaxcala Al Descubierto">T A D</span></h6>
+            </nav>
+        </>
+    );
+}
--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -15,6 +15,8 @@ import type { Viewport } from 'next';
 import { FiHeart } from "react-icons/fi";
 // Menu principal
 import MenuPrincipal from "../components/MenuPrincipal";
+// Header superior
+import HeaderSuperior from "../components/HeaderSuperior";
 // Modal toldo
 import ModalOpciones from "../components/inicio/ModalOpciones";
 
@@ -47,12 +49,9 @@ export default function Inicio() {
 				<div className="col-md-10 col-lg-10  col-xl-6 contenedor_central_contenido">
 					{/* <!--Contenedor menu prinicpal--> */}
 						<div className="contenedor_menu_superior sticky-top">
-							{/* <!--Menu superior--> */}
-							<nav className="navbar-expand-lg menu_superior_central">
-								<h1 className="titulo_principal_h1">TLAXCALA EN <span className="display_inline_titulo">IMÁGENES</span></h1>
-								<h6 className="subtitulo_h6 por_tad">por <span className="span_tad d-block" data-bs-toggle="tooltip" data-bs-placement="right" title="Tlaxcala Al Descubierto">T A D</span></h6>
-							</nav>
-							{/* <!--Fin Menu superior--> */}
+							{/* <!--Header Superior--> */}
+							<HeaderSuperior />
+							{/* <!--Fin Header Superior--> */}
 						</div>
 					{/* <!--Fin Contenedor menu prinicpal--> */}
 					
