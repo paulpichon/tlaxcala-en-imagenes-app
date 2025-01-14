fbc105bc526e8dc9b18bb656de02abe7610779f9
paul pichon <paul10_barca@hotmail.com>
2025-01-14 14:30:09 -0600

    feat: creacion de archivo types, donde se crea la interface de datos para imagenes para la pagina perfil del usuario

---
+++ src/app/types/types.ts
@@ -0,0 +1,8 @@
+// Interface de datos de imagenes para perfil de usuario
+// Estos datos deben de ser cambios despues por los datos traidos por la API
+export default interface ImageData {
+    id: number;
+    src: string;
+    title: string;
+    description: string;
+}
