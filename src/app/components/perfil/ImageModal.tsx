821888c021119ac2849ef59e6c329ed5de00c0aa
paul pichon <paul10_barca@hotmail.com>
2025-01-15 14:24:32 -0600

    refactor: diseño del modal abierto

--- src/app/components/perfil/ImageModal.tsx
+++ src/app/components/perfil/ImageModal.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 // Types/interface
 import ImageData from "@/app/types/types";
+// Icono
+import { FiHeart, FiMoreHorizontal } from "react-icons/fi";
 // Interface props del modal
 interface ImageModalProps {
   image: ImageData;
@@ -15,33 +17,39 @@ export default function ImageModal({ image, onClose }: ImageModalProps) {
       tabIndex={-1}
       style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
     >
-      <div className="modal-dialog modal-lg modal-dialog-centered">
+      <div className="modal-dialog modal-dialog-centered modal-lg">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title">{image.title}</h5>
+            {/* <h5 className="modal-title">{image.title}</h5> */}
             <button
               type="button"
-              className="btn-close"
-              onClick={onClose}
-              aria-label="Close"
-            ></button>
+              className="btn_opciones_modal"
+              aria-label="Options"
+            >
+              <FiMoreHorizontal />
+            </button>
+            {/* Boton para cerrar la ventana */}
+            <button type="button" className="btn-close" onClick={onClose} aria-label="Close"></button>
           </div>
-          <div className="modal-body">
+          <div className="contenedor_imagen_expandida">
             <Image
               src={image.src}
               alt={image.title}
               width={800}
               height={600}
-              className="img-fluid rounded mb-3"
+              className="img-fluid"
               style={{ objectFit: "cover" }}
             />
-            <p>{image.description}</p>
+            {/* <p>{image.description}</p> */}
           </div>
-          <div className="modal-footer">
-            <button className="btn btn-secondary" onClick={onClose}>
-              Cerrar
+          <div className="footer_publicacion">
+            <button id="likeButton" data-post-id="POST_ID" className="like-button">
+                {/* <!--https://feathericons.com/--> */}
+                <FiHeart />
             </button>
-          </div>
+            <p id="likeCount" className="d-inline votaciones">10009</p>
+            <strong>Me gusta</strong>
+		   		</div>
         </div>
       </div>
     </div>
