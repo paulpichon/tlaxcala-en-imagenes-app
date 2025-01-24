2ba1080093fee5c7988835fa1be98e3ed0cbfc71
paul pichon <paul10_barca@hotmail.com>
2025-01-23 21:32:05 -0600

    refactor: se añade globals,css

--- src/app/perfil/layout.tsx
+++ src/app/perfil/layout.tsx
@@ -1,7 +1,8 @@
 // Inicio
 // Se usara Layouts Anidados en la estructura del proyecto
 // https://chatgpt.com/c/674542f3-d5bc-8002-847c-c817ca29ba97
-
+// Link_activo: color del link activo
+import "../ui/globals.css";
 // Meta datos NEXTJS
 import { Metadata } from "next";
 // Fonts
