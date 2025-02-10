0af501e924c2923e3e16584e5c6e2e689484a2be
paul pichon <paul10_barca@hotmail.com>
2025-02-10 13:18:59 -0600

    refactor: se quito css/globals

--- src/app/inicio/layout.tsx
+++ src/app/inicio/layout.tsx
@@ -1,9 +1,6 @@
 // Inicio
 // Se usara Layouts Anidados en la estructura del proyecto
 // https://chatgpt.com/c/674542f3-d5bc-8002-847c-c817ca29ba97
-// import GLOBALS
-// Link_activo: color del link activo
-import "../ui/globals.css";
 
 // Meta datos NEXTJS
 import { Metadata } from "next";
