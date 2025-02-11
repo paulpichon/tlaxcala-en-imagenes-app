18355b3798d01e5479c816e8331da09267c9f962
paul pichon <paul10_barca@hotmail.com>
2025-02-11 16:42:28 -0600

    refactor: se cammbia el nombre del archivo

---
+++ src/app/components/HeaderPrincipalTei.tsx
@@ -0,0 +1,11 @@
+// Componente header de index, cuentas, crear-cuenta, login de TLAXCALA EN IMAGENES por TAD
+export function HeaderPrincipalTei() {
+    return (
+        <div className="col-md-12">
+            <div className="contenedor_tei_index">
+                <h1 className="titulo_principal_h1">TLAXCALA EN <span className="d-block">IMÁGENES</span></h1>
+                <h6 className="subtitulo_h6">por <span className="span_tad d-block">T A D</span></h6>
+            </div>
+		</div>
+    );
+}
