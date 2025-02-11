acd3f343c6d8c82d42536b38c109e970a02fb1ba
paul pichon <paul10_barca@hotmail.com>
2025-02-11 16:47:48 -0600

    docs: comentarios y indentacion

--- src/app/components/HeaderSuperior.tsx
+++ src/app/components/HeaderSuperior.tsx
@@ -1,10 +1,18 @@
+// Header superior de las páginas principales de la APP: inicio, perfil, notificaciones, etc etc
 'use client';
 export default function HeaderSuperior() {
     return (
         <>
             <nav className="navbar-expand-lg menu_superior_central">
-                <h1 className="titulo_principal_h1">TLAXCALA EN <span className="display_inline_titulo">IMÁGENES</span></h1>
-                <h6 className="subtitulo_h6 por_tad">por <span className="span_tad d-block" data-bs-toggle="tooltip" data-bs-placement="right" title="Tlaxcala Al Descubierto">T A D</span></h6>
+                <h1 className="titulo_principal_h1">
+                    TLAXCALA EN 
+                    <span className="display_inline_titulo">IMÁGENES</span>
+                </h1>
+                <h6 className="subtitulo_h6 por_tad">por 
+                    <span className="span_tad d-block" data-bs-toggle="tooltip" data-bs-placement="right" title="Tlaxcala Al Descubierto">
+                        T A D
+                    </span>
+                </h6>
             </nav>
         </>
     );
