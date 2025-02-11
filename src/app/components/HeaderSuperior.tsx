3aca8e4adeccd7d3cb13362df0ef22b62a91a5b2
paul pichon <paul10_barca@hotmail.com>
2025-02-11 16:49:22 -0600

    style: header principal de la APP

--- src/app/components/HeaderSuperior.tsx
+++ src/app/components/HeaderSuperior.tsx
@@ -6,7 +6,7 @@ export default function HeaderSuperior() {
             <nav className="navbar-expand-lg menu_superior_central">
                 <h1 className="titulo_principal_h1">
                     TLAXCALA EN 
-                    <span className="display_inline_titulo">IMÁGENES</span>
+                    <span className="display_inline_titulo"> IMÁGENES</span>
                 </h1>
                 <h6 className="subtitulo_h6 por_tad">por 
                     <span className="span_tad d-block" data-bs-toggle="tooltip" data-bs-placement="right" title="Tlaxcala Al Descubierto">
