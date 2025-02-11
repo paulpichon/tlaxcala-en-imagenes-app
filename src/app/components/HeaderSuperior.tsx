e21bdb85723f78b8939b9849ea015698df6fc7ee
paul pichon <paul10_barca@hotmail.com>
2025-02-11 17:24:47 -0600

    refactor: se cambian nombre de clases css

--- src/app/components/HeaderSuperior.tsx
+++ src/app/components/HeaderSuperior.tsx
@@ -4,12 +4,12 @@ export default function HeaderSuperior() {
     return (
         <>
             <nav className="navbar-expand-lg menu_superior_central">
-                <h1 className="titulo_principal_h1">
+                <h1 className="titulo_header_superior_app">
                     TLAXCALA EN 
                     <span className="display_inline_titulo"> IMÁGENES</span>
                 </h1>
-                <h6 className="subtitulo_h6 por_tad">por 
-                    <span className="span_tad d-block" data-bs-toggle="tooltip" data-bs-placement="right" title="Tlaxcala Al Descubierto">
+                <h6 className="mostrar_h6_por">por 
+                    <span className="span_tad_header_app d-block" data-bs-toggle="tooltip" data-bs-placement="right" title="Tlaxcala Al Descubierto">
                         T A D
                     </span>
                 </h6>
