8649bb4e20c4dcb55dcb006522fb44999ed42814
paul pichon <paul10_barca@hotmail.com>
2025-02-19 18:51:17 -0600

    refactor: se quita el llamadao de archivo css en page

--- src/app/cuentas/confirmacion/correo-enviado/page.tsx
+++ src/app/cuentas/confirmacion/correo-enviado/page.tsx
@@ -2,13 +2,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 // Componente
 import CorreoEnviado from "../components/CorreoEnviado";
-// estilos de la pagina son iguales para correo-enviado y para correo-enviado.restablecer-password
-import "../../../ui/cuentas/confirmacion/correo-enviado/correo-enviado.css";
 
 export default function CrearCuenta() {
     return (
 		<div className="container-fluid container-xl">
-            <div className="row justify-content-center contenedor_principal">
+            <div className={`row justify-content-center contenedor_principal`}>
                <CorreoEnviado />
             </div>
         </div>
