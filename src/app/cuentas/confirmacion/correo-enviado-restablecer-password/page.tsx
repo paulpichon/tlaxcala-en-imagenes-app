64f374995668f54ed09d0c411465c6c4882051a6
paul pichon <paul10_barca@hotmail.com>
2025-02-19 19:15:21 -0600

    styles: se quita llamado a css, se cambia por CSS modules

--- src/app/cuentas/confirmacion/correo-enviado-restablecer-password/page.tsx
+++ src/app/cuentas/confirmacion/correo-enviado-restablecer-password/page.tsx
@@ -2,8 +2,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 // Componente
 import CorreoEnviado from "../components/CorreoEnviado";
-// estilos de la pagina, seran compartidos con la página correo-enviado
-import "../../../ui/cuentas/confirmacion/correo-enviado/correo-enviado.css";
 
 export default function CrearCuenta() {
   return (
