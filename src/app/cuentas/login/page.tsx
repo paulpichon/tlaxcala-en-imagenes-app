7a3fcae44d94bbca8b90cb064b04608345d831cd
paul pichon <paul10_barca@hotmail.com>
2025-02-24 12:51:46 -0600

    refactor: se cambian las clases: modules css

--- src/app/cuentas/login/page.tsx
+++ src/app/cuentas/login/page.tsx
@@ -6,7 +6,7 @@ import type { Viewport } from 'next';
 // bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 // Estilos de pagina
-import "../../ui/cuentas/login/login.css";
+import login from "../../ui/cuentas/login/login.module.css";
 // Header principal
 import { HeaderPrincipalTei } from "@/app/components/HeaderPrincipalTei";
 // Footer principal
@@ -29,23 +29,23 @@ export default function IniciarSesion() {
 				{/* Header principal */}
                 <HeaderPrincipalTei />
 				<div className="col-sm-9 col-md-7 col-lg-6">
-					<div className="contenedor_formulario ">
-						<div className="contenedor_titulos">
-							<h3 className="subtitulo_h3">Iniciar sesión</h3>
+					<div className={`${login.contenedor_formulario}`}>
+						<div className={`${login.contenedor_titulos}`}>
+							<h3 className={`${login.subtitulo_h3}`}>Iniciar sesión</h3>
 						</div>
-						<div className="contenedor_formulario">
+						<div className={`${login.contenedor_formulario}`}>
 							<form className="formulario_crear_cuenta" id="iniciar_sesion">
-								<div className="contenedor_inputs_login">
-								  <input type="text" className="form-control inputs_crear_cuenta" id="correo" aria-describedby="correo" placeholder="Correo electrónico" />
+								<div className={`${login.contenedor_inputs_login}`}>
+								  <input type="text" className={`form-control ${login.inputs_crear_cuenta}`} id="correo" aria-describedby="correo" placeholder="Correo electrónico" />
 								</div>
-								<div className="contenedor_inputs_login">
-								  <input type="password" className="form-control inputs_crear_cuenta" id="password" placeholder="Contraseña" />
+								<div className={`${login.contenedor_inputs_login}`}>
+								  <input type="password" className={`form-control ${login.inputs_crear_cuenta}`} id="password" placeholder="Contraseña" />
 								</div>
-								<button type="submit" className="boton_registrarse">Iniciar sesión</button>
+								<button type="submit" className={`${login.boton_registrarse}`}>Iniciar sesión</button>
 							</form>
                             <div className="cool-md-12 text-center">
-                                <p className="pregunta">
-                                    <Link href="/cuentas/login/password-olvidado" className="enlace_recuperar_password">
+                                <p className={`${login.pregunta}`}>
+                                    <Link href="/cuentas/login/password-olvidado" className={`${login.enlace_recuperar_password}`}>
                                     ¿Se te olvidó la contraseña?
                                     </Link>
                                 </p>
