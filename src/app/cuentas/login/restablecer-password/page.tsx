465a9d13782f050b8fda5670c0ad59d23f6152ca
paul pichon <paul10_barca@hotmail.com>
2025-02-24 12:27:24 -0600

    refactor: se cambian las clases de css: modules css

--- src/app/cuentas/login/restablecer-password/page.tsx
+++ src/app/cuentas/login/restablecer-password/page.tsx
@@ -2,7 +2,7 @@
 // bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 // Estilos de pagina
-import "../../../ui/cuentas/login/restablecer-password/restablecer-password.css";
+import restaPasssword from "../../../ui/cuentas/login/restablecer-password/RestablecerPassword.module.css";
 // Header principal
 import { HeaderPrincipalTei } from "@/app/components/HeaderPrincipalTei";
 // Footer principal
@@ -25,17 +25,17 @@ export default function RestablecerPassword() {
 			<div className="row justify-content-center contenedor_principal">
 				<HeaderPrincipalTei />
 				<div className="col-sm-9 col-md-7 col-lg-6">
-					<div className="contenedor_formulario ">
-						<div className="contenedor_titulos">
-							<h3 className="subtitulo_h3">Reestablecer contraseña</h3>
-                            <p className="texto">Introduce tu nueva contraseña</p>
+					<div className={`${restaPasssword.contenedor_formulario}`}>
+						<div className={`${restaPasssword.contenedor_titulos}`}>
+							<h3 className={`${restaPasssword.subtitulo_h3}`}>Reestablecer contraseña</h3>
+                            <p className={`${restaPasssword.texto}`}>Introduce tu nueva contraseña</p>
 						</div>
-						<div className="contenedor_formulario">
+						<div className={`${restaPasssword.contenedor_formulario}`}>
 							<form className="formulario_crear_cuenta" id="iniciar_sesion">
 								<div className="mb-4">
-									<input type="password" className="form-control inputs_crear_cuenta" id="password" placeholder="Nueva contraseña" />
+									<input type="password" className={`form-control ${restaPasssword.inputs_crear_cuenta}`} id="password" placeholder="Nueva contraseña" />
 								</div>
-								<button type="submit" className="boton_registrarse">Confirmar</button>
+								<button type="submit" className={`${restaPasssword.boton_registrarse}`}>Confirmar</button>
 							  </form>
 						</div>
 					</div>
