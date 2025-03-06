bf8d2fe23cbc8e5ba7a4ce0a36f931cbcf0b258e
paul pichon <paul10_barca@hotmail.com>
2025-03-06 12:06:40 -0600

    feat: se crea interface para formulario de registro

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,16 @@
 // Interface de datos de imagenes para perfil de usuario
 // Estos datos deben de ser cambios despues por los datos traidos por la API
-export default interface ImageData {
+export interface ImageData {
     id: number;
     src: string;
     title: string;
     description: string;
+}
+// Interface para definir los tipos de datos del formulario de registro
+// Interface
+export interface IUsuarioData {
+	nombre: string;
+	apellido: string;
+	correo: string;
+	password: string;
 }
