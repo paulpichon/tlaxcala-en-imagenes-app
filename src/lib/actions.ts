1f4f4ebed05cf6e3dc7b564c0a9e42e08ba565f2
paul pichon <paul10_barca@hotmail.com>
2025-03-06 20:40:56 -0600

    feat: funcino para crear usuario con la API, falta mejorar algunas cosas como la URL de la API, debemos llamar dinamicamente

---
+++ src/lib/actions.ts
@@ -0,0 +1,30 @@
+// Funcion para crear el usuario
+'use server';
+// interface para tipo de datos de formulario de registro
+import { IUsuarioData } from "@/types/types";
+export async function createUsuario( formData: IUsuarioData) {
+    // desestructuracion de formData
+    const {nombre, apellido, correo, password} = formData;
+    // headers
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    // creamos el raw
+    const raw = JSON.stringify({
+        "nombre_completo": {
+            "nombre": nombre,
+            "apellido": apellido
+        },
+        "correo": correo,
+        "password": password
+    });
+    // Fetch a la API
+    const response = await fetch("https://login-autenticacion.onrender.com/api/usuarios", {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+    });
+    // convertimos la respuesta en json
+    const respuesta = await response.json();
+    // retornamos la respuesta para obtener el JSON que viene desde la API
+    return respuesta;
+}
