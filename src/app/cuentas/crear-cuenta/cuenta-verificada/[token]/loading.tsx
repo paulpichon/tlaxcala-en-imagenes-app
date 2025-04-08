c814aefdd3337b3278686aef91bb7eb74eebf138
paul pichon <paul10_barca@hotmail.com>
2025-04-07 18:10:19 -0600

    refactor: se añade comentarios en PAGE loading

--- src/app/cuentas/crear-cuenta/cuenta-verificada/[token]/loading.tsx
+++ src/app/cuentas/crear-cuenta/cuenta-verificada/[token]/loading.tsx
@@ -1,3 +1,4 @@
+// Esta pagina de loading, podri verse afectada porque RENDER se desactiva cada cierto tiempo,  entonces podria pasar que esta pagina se quede pegada cuando esta cargando, ya que al hacer la consulta de la API esta RENDER podria estar desactivada, pero de no estarlo deberia funciona normal.
 export default function Loading() {
     return (
         <div className="d-flex vh-100 justify-content-center align-items-center">
