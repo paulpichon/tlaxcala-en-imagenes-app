cbf9a0979bc3eeab440b21e20f60a9ce4e1b0ac9
paul pichon <paul10_barca@hotmail.com>
2025-04-14 23:37:31 -0600

    refactor: se quito la interface, para ponerla en el archivo de types

--- src/app/cuentas/confirmacion/components/ModalReenviarCorreo.tsx
+++ src/app/cuentas/confirmacion/components/ModalReenviarCorreo.tsx
@@ -1,16 +1,7 @@
+// Componente para: reenvio de correo electronico
 'use client';
-
-interface ModalReenviarCorreoProps {
-    show: boolean;
-    onClose: () => void;
-    onReenviar: () => void;
-    estilos: { [key: string]: string };
-    mensaje?: string | null;
-    // reenviando?: boolean;
-    esExito?: boolean | null;
-    bloqueado?: boolean;
-    cuentaVerificada?: boolean;
-  }
+// Interface ModalReenviarCorreoProps
+import { ModalReenviarCorreoProps } from "@/types/types";
 
 const ModalReenviarCorreo: React.FC<ModalReenviarCorreoProps> = ({
     show,
@@ -18,67 +9,68 @@ const ModalReenviarCorreo: React.FC<ModalReenviarCorreoProps> = ({
     onReenviar,
     estilos,
     mensaje,
-    // reenviando,
     esExito,
     bloqueado,
     cuentaVerificada
 }) => {
-    
+    // Modal
     if (!show) return null;
 
-    //mensaje
+    //mensaje cambia color dependiendo de la respuesta de la API
     const mensajeEstilo = esExito === null
     ? ''
     : esExito
-      ? { color: 'green' }
-      : { color: 'red' };   
+        ? { color: 'green' }
+        : { color: 'red' };   
 
-    
-
-  return (
-    <div
-      className={`modal fade ${show ? 'show' : ''}`}
-      tabIndex={-1}
-      style={{
-        display: show ? 'block' : 'none',
-        backgroundColor: 'rgba(0,0,0,0.5)',
-      }}
-      role="dialog"
-    >
-      <div className="modal-dialog modal-dialog-centered">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h1 className="modal-title fs-5" id="exampleModalLabel">
-              ¿No recibiste el correo electrónico?
-            </h1>
-            <button
-              type="button"
-              className="btn-close"
-              aria-label="Close"
-              onClick={onClose}
-            ></button>
-          </div>
-            <div className="modal-body text-center">
-                {!cuentaVerificada && (
-                    <button
-                    id="reenviar_correo"
-                    className={`btn ${estilos.boton_reenviar_correo}`}
-                    onClick={onReenviar}
-                    disabled={bloqueado}
-                    >
-                    {bloqueado
-                        ? "Espera 5 minutos para volver a reenviar el correo..."
-                        : "Reenviar correo electrónico"}
-                    </button>
-                )}
-                {mensaje && (
-                    <p style={{ marginTop: '1rem', ...mensajeEstilo }}>{mensaje}</p>
-                )} 
+    return (
+        <div
+        className={`modal fade ${show ? 'show' : ''}`}
+        tabIndex={-1}
+        style={{
+            display: show ? 'block' : 'none',
+            backgroundColor: 'rgba(0,0,0,0.5)',
+        }}
+        role="dialog"
+        >
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="exampleModalLabel">
+                        ¿No recibiste el correo electrónico?
+                        </h1>
+                        <button
+                        type="button"
+                        className="btn-close"
+                        aria-label="Close"
+                        onClick={onClose}
+                        ></button>
+                    </div>
+                    <div className="modal-body text-center">
+                        {!cuentaVerificada && (
+                            // Si la cuenta ya esta verificada, se quita el boton de reenvio de correo 
+                            <button
+                                id="reenviar_correo"
+                                className={`btn ${estilos.boton_reenviar_correo}`}
+                                onClick={onReenviar}
+                                disabled={bloqueado}
+                            >
+                                {bloqueado
+                                    ? "Espera 5 minutos para volver a reenviar el correo..."
+                                    : "Reenviar correo electrónico"
+                                }
+                            </button>
+                        )}
+                        {mensaje && (
+                            // Si hay mensaje, se muestra
+                            // Se cambia el color del mensaje dependiendo de la respuesta de la API
+                            <p style={{ marginTop: '1rem', ...mensajeEstilo }}>{mensaje}</p>
+                        )} 
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ModalReenviarCorreo;
