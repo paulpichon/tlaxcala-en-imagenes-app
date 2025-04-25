ca87a28d13c4e4c8874435ec861370a511bdeec0
paul pichon <paul10_barca@hotmail.com>
2025-04-25 01:27:59 -0600

    feat: se añade un .pick de usuarioschema

--- src/lib/validaciones.ts
+++ src/lib/validaciones.ts
@@ -6,9 +6,12 @@ import { z } from "zod";
 export const usuarioSchema = z.object({
     nombre: z.string().min(1, { message: "El nombre es requerido" }),
     apellido: z.string().min(1, { message: "El apellido es requerido" }),
-    correo: z.string().email({ message: "Correo electrónico inválido" }),
+    correo: z.string().email({ message: "Ingresa un correo válido" }),
     password: z.string().min(6, { message: "La contraseña debe tener al menos 6 caracteres" })
-  });
-  
-  // Tipo derivado del esquema
-  export type UsuarioSchema = z.infer<typeof usuarioSchema>;
+});
+// Tipo derivado del esquema
+export type UsuarioSchema = z.infer<typeof usuarioSchema>;
+
+// Validar el input para envio de correo para restablecer la contraseña 
+// extencion del esquema de usuarioSchema
+export const correoSchema = usuarioSchema.pick({ correo: true });
