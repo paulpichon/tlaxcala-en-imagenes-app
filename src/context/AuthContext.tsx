1b24146c9522ab4d981106530544aa0968cd8408
paul pichon <paul10_barca@hotmail.com>
2025-05-28 16:46:08 -0600

    refactor: se mueven las interfaces IUserLogin y IAuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,23 +1,22 @@
 'use client';
-import { createContext, useContext, useEffect, useState } from 'react';
-// Tipos de dato para el usuario
-type User = {
-  nombre: string;
-  correo: string;
-};
-// Tipo de datos para el contexto de autenticación
-type AuthContextType = {
-  user: User | null;
-  loading: boolean;
-  login: (user: User) => void;
-  logout: () => void;
-};
+// Importa los hooks necesarios de React para crear un contexto y manejar el estado.
+import {    createContext, 
+            useContext, 
+            useEffect, 
+            useState 
+        } from 'react';
+// Interface IuserLogin define la estructura de los datos del usuario que se almacenarán en el contexto.
+// Interface para el contexto de autenticación, define las propiedades que estarán disponibles en el contexto.
+import {    IUserLogin, 
+            IAuthContext 
+        } from '@/types/types';
+        
 // Se crea el contexto, inicialmente sin valor. Se usará para compartir los datos de autenticación entre componentes.
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+const AuthContext = createContext<IAuthContext | undefined>(undefined);
 // Este componente envolverá toda la app, permitiendo que cualquier componente acceda al estado de autenticación.
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     // user: representa al usuario logueado.
-    const [user, setUser] = useState<User | null>(null);
+    const [user, setUser] = useState<IUserLogin | null>(null);
     // loading: indica si se está cargando la sesión del usuario.
     const [loading, setLoading] = useState(true);
     
@@ -58,7 +57,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         fetchUser();
     }, []);
     // Actualiza el estado user. Útil para cuando el usuario inicia sesión manualmente.
-    const login = (user: User) => {
+    const login = (user: IUserLogin) => {
         setUser(user);
     };
     // Llama al endpoint /logout del backend y borra el usuario en frontend.
