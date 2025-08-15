c3c75c8b24e919b73938c8e504887af47ee74417
Paúl Pichón <paul10_barca@hotmail.com>
2025-08-14 19:57:22 -0600

    feat: se añade configuracion para permitir imagenes externas en este caso de clodinary

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Permitir imagenes de Cloudinary
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/dy9prn3ue/**', // tu carpeta en Cloudinary
+      },
+    ],
+  },
 };
 
 export default nextConfig;
