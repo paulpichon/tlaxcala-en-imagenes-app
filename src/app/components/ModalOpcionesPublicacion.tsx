64f727a9a3cdf414861fcd04e737f25cdda8f226
Paúl Pichón <paul10_barca@hotmail.com>
2025-08-28 21:09:40 -0600

    refactor: se añade el componente FollowButton

--- src/app/components/ModalOpcionesPublicacion.tsx
+++ src/app/components/ModalOpcionesPublicacion.tsx
@@ -1,81 +1,10 @@
 'use client';
 
-import { useState, useEffect } from "react";
 import { PropsModalOpcionesPublicacion } from "@/types/types";
-import { useAuth } from "@/context/AuthContext";
-// Module CSS
 import perfil from "../ui/perfil/perfil.module.css";
-import Spinner from "./spinner";
+import FollowButton from "./FollowButton";
 
 const ModalOpcionesPublicacion: React.FC<PropsModalOpcionesPublicacion> = ({ isOpen, selectedImage, onClose }) => {
-  const { fetchWithAuth } = useAuth();
-  const [isFollowing, setIsFollowing] = useState(false); 
-  const [loading, setLoading] = useState(false);
-  const [checkingFollow, setCheckingFollow] = useState(true); // para evitar parpadeo
-
-  // ✅ Al abrir el modal, consultamos si YA sigue al usuario
-  useEffect(() => {
-    const checkIfFollowing = async () => {
-      if (!isOpen || !selectedImage?._idUsuario) return;
-      setCheckingFollow(true);
-
-    	try {
-			const res = await fetchWithAuth(
-			`${process.env.NEXT_PUBLIC_API_URL_LOCAL}/api/followers/is-following/${selectedImage._idUsuario._id}`
-			);
-			if (res.ok) {
-			const data = await res.json();
-			
-			setIsFollowing(data.sigueUsuario); // true o false
-			} else {
-			console.warn("No se pudo verificar follow");
-			setIsFollowing(false);
-			}
-		} catch (error) {
-			console.error("Error verificando follow:", error);
-			setIsFollowing(false);
-		} finally {
-			setCheckingFollow(false);
-		}
-    };
-
-    checkIfFollowing();
-  }, [isOpen, selectedImage?._idUsuario, fetchWithAuth]);
-
-  const handleToggleFollow = async () => {
-    if (!selectedImage?._idUsuario) return;
-    setLoading(true);
-
-    try {
-      let res;
-      if (isFollowing) {
-        // 🔴 Dejar de seguir
-        res = await fetchWithAuth(
-          `${process.env.NEXT_PUBLIC_API_URL_LOCAL}/api/followers/unfollow/${selectedImage._idUsuario._id}`,
-          {
-            method: "DELETE",
-          }
-        );
-      } else {
-        // 🟢 Seguir
-        res = await fetchWithAuth(
-          `${process.env.NEXT_PUBLIC_API_URL_LOCAL}/api/followers/follow/${selectedImage._idUsuario._id}`,
-          {
-            method: "POST",
-          }
-        );
-      }
-
-      	if (!res.ok) throw new Error("Error en follow/unfollow");
-    	// const data = await res.json();
-    	await res.json();
-      	setIsFollowing(!isFollowing);
-    } catch (error) {
-      console.error("Error follow/unfollow:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   if (!isOpen || !selectedImage) return null;
 
@@ -90,21 +19,9 @@ const ModalOpcionesPublicacion: React.FC<PropsModalOpcionesPublicacion> = ({ isO
           <div className="modal-body">
             <div className="row text-center">
               <div className="col-md-12">
-                <button
-                  id="seguir_usuario"
-                  type="button"
-                  disabled={loading || checkingFollow}
-                  className={` ${perfil.btn_opciones_publicaciones} ${isFollowing ? perfil.btn_rojo : perfil.btn_seguir}`}
-                  onClick={handleToggleFollow}
-                >
-                  {loading
-                    ? <Spinner size="20px" />
-                    : checkingFollow
-                    ? <Spinner size="20px" />
-                    : isFollowing
-                    ? "Dejar de seguir"
-                    : "Seguir"}
-                </button>
+              {/* Componente buton seguir/dejar de seguir */}
+              <FollowButton userId={selectedImage._idUsuario._id} />
+              {/* Componente buton seguir/dejar de seguir */}
               </div>
               <div className="col-md-12">
                 <a type="button" className={`${perfil.btn_opciones_publicaciones}`} href="#">
@@ -116,7 +33,6 @@ const ModalOpcionesPublicacion: React.FC<PropsModalOpcionesPublicacion> = ({ isO
                   Ir a la publicación
                 </a>
               </div>
-			  
               <div className="col-md-12">
                 <a
                   type="button"
