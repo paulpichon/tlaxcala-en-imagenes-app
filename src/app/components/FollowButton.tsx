6f3c994991dd6741744347e7c660720e2d1930f5
Paúl Pichón <paul10_barca@hotmail.com>
2025-09-08 20:11:18 -0600

    refactor: funcion para seguir/eliminar usuario

--- src/app/components/FollowButton.tsx
+++ src/app/components/FollowButton.tsx
@@ -2,32 +2,47 @@
 
 import perfil from "../ui/perfil/perfil.module.css";
 import Spinner from "./spinner";
-import { useFollow } from "../hooks/useFollow";
+import { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
+import { FollowButtonProps } from "@/types/types";
 
-interface FollowButtonProps {
-  userId: string;
-  isOpen: boolean;
-}
+const FollowButton: React.FC<FollowButtonProps> = ({ userId, initialFollowing, onToggle }) => {
+  const { fetchWithAuth } = useAuth();
+  const [isFollowing, setIsFollowing] = useState(initialFollowing);
+  const [loading, setLoading] = useState(false);
 
-const FollowButton: React.FC<FollowButtonProps> = ({ userId, isOpen }) => {
-  const { isFollowing, loading, checkingFollow, toggleFollow } = useFollow(userId, isOpen);
+  const toggleFollow = async () => {
+    setLoading(true);
+    try {
+      let res;
+      if (isFollowing) {
+        res = await fetchWithAuth(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}/api/followers/unfollow/${userId}`, { method: "DELETE" });
+      } else {
+        res = await fetchWithAuth(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}/api/followers/follow/${userId}`, { method: "POST" });
+      }
+
+      if (res.ok) {
+        const newState = !isFollowing;
+        setIsFollowing(newState);
+        onToggle?.(newState); // 👈 notifica al hook
+      }
+    } catch (error) {
+      console.error("Error follow/unfollow:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <button
       type="button"
-      disabled={loading || checkingFollow}
+      disabled={loading}
       className={`${perfil.btn_opciones_publicaciones} ${
         isFollowing ? perfil.btn_rojo : perfil.btn_seguir
       }`}
       onClick={toggleFollow}
     >
-      {loading || checkingFollow ? (
-        <Spinner size="20px" />
-      ) : isFollowing ? (
-        "Dejar de seguir"
-      ) : (
-        "Seguir"
-      )}
+      {loading ? <Spinner size="20px" /> : isFollowing ? "Dejar de seguir" : "Seguir"}
     </button>
   );
 };
