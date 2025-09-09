2e981be83053324f05afa0061a8c062f6c6a67a0
Paúl Pichón <paul10_barca@hotmail.com>
2025-09-08 20:12:54 -0600

    refactor: funcion para agregar/eliminar favorito

--- src/app/components/FavoritoButton.tsx
+++ src/app/components/FavoritoButton.tsx
@@ -1,40 +1,59 @@
 'use client';
 
-import { useFavorito } from "../hooks/useFavorito";
 import perfil from "../ui/perfil/perfil.module.css";
 import Spinner from "./spinner";
+import { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
+import { FavoritoButtonProps } from "@/types/types";
 
-interface Props {
-  posteoId: string;
-  autorId: string;
-  imagenUrl: string;
-  isOpen: boolean;
-}
 
-const FavoritoButton: React.FC<Props> = ({ posteoId, autorId, imagenUrl, isOpen }) => {
-  const { esFavorito, loadingFav, loadingCheckFav, toggleFavorito } = useFavorito(
-    posteoId,
-    autorId,
-    imagenUrl,
-    isOpen,
-  );
+const FavoritoButton: React.FC<FavoritoButtonProps> = ({ posteoId, autorId, imagenUrl, initialFavorito, onToggle, }) => {
+  const { fetchWithAuth } = useAuth();
+  const [esFavorito, setEsFavorito] = useState(initialFavorito);
+  const [loading, setLoading] = useState(false);
+  
+  const toggleFavorito = async () => {
+    setLoading(true);
+    try {
+      // Aquí va tu llamada al backend (añadir/quitar favorito)
+      let res;
+      if (esFavorito) {
+        res = await fetchWithAuth(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}/api/favoritos/${posteoId}`, { method: "DELETE" });
+      } else {
+        res = await fetchWithAuth(`${process.env.NEXT_PUBLIC_API_URL_LOCAL}/api/favoritos/${posteoId}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ imagenUrl, autorId }),
+        });        
+      }
+
+      if (res.ok) {
+        const data = await res.json();
+        if (data.msg === "Agregado en Favoritos") {
+          setEsFavorito(true);
+          onToggle?.(true);
+        } else if (data.msg === "Eliminado de Favoritos") {
+          setEsFavorito(false);
+          onToggle?.(false);
+        }
+      }
+    } catch (err) {
+      console.error("Error en toggleFavorito:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <button
       type="button"
-      disabled={loadingFav || loadingCheckFav}
+      disabled={loading}
       className={`${perfil.btn_opciones_publicaciones}`}
       onClick={toggleFavorito}
     >
-      {loadingFav || loadingCheckFav ? (
-        <Spinner size="20px" />
-      ) : esFavorito ? (
-        "Quitar de favoritos"
-      ) : (
-        "Añadir a favoritos"
-      )}
+      {loading ? <Spinner size="20px" /> : esFavorito ? "Quitar de favoritos" : "Añadir a favoritos"}
     </button>
   );
-}
+};
 
 export default FavoritoButton;
