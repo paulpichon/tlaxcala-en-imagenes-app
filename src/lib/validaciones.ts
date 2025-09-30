072f806ce86a1459fa9e3cc43430dcbaf1c9f552
Paúl Pichón <paul10_barca@hotmail.com>
2025-09-29 19:17:10 -0600

    feat: se crea validacion para la creacion de un POSTEO con ZOD

--- src/lib/validaciones.ts
+++ src/lib/validaciones.ts
@@ -2,7 +2,7 @@
 // Zod
 import { z } from "zod";
 
-// Esquema de validación para el formulario de registro
+//* Esquema de validación para el formulario de registro
 export const usuarioSchema = z.object({
     nombre: z.string().min(1, { message: "El nombre es requerido" }),
     apellido: z.string().min(1, { message: "El apellido es requerido" }),
@@ -18,7 +18,7 @@ export const correoSchema = usuarioSchema.pick({ correo: true });
 // Validar la contraseña de la pagina para restablecer contraseña
 // extension del esquema de usuarioSchema
 export const passwordSchema = usuarioSchema.pick({ password: true });
-// Confirmar la contraseña
+//* Confirmar la contraseña
 // extension del esquema de usuarioSchema
 export const resetPasswordSchema = passwordSchema.extend({
 	confirmPassword: z.string(),
@@ -29,3 +29,40 @@ export const resetPasswordSchema = passwordSchema.extend({
 // tipo de resetPasswordSchema
 // Restablecer contraseña
 export type ResetPasswordSchema = z.infer<typeof resetPasswordSchema>;
+
+// Esquema base sin refine
+export const posteoBaseSchema = z.object({
+    texto: z
+    .string()
+    .max(200, { message: "La descripción no puede superar los 200 caracteres" })
+    .regex(/^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ.,!?¡¿()\s-]*$/, {
+      message: "La descripción contiene caracteres no permitidos",
+    })
+    .optional(),
+    file: z
+        .instanceof(File)
+        .refine((file) => file.size <= 5 * 1024 * 1024, {
+            //? Este mensaje de error debe ser igual al que se ponga en el archivo CrearPosteoModal.tsx, de lo contrario prodria crearse un error
+        message: "La imagen no debe superar los 5 MB",
+        })
+        .refine(
+        (file) => ["image/jpeg", "image/jpg", "image/webp"].includes(file.type),
+        {
+            //? Este mensaje de error debe ser igual al que se ponga en el archivo CrearPosteoModal.tsx, de lo contrario prodria crearse un error
+            message: "No se admite este archivo. Solo se permiten .jpg, .jpeg y .webp",
+        }
+        )
+        .optional(),
+    posteo_publico: z.boolean(),
+});
+
+// Esquema completo con la regla extra
+export const posteoSchema = posteoBaseSchema.refine(
+  (data) => data.texto?.trim() !== "" || data.file,
+  {
+    message: "Debes escribir una descripción o subir una imagen",
+    path: ["file"],
+  }
+);
+
+export type PosteoSchema = z.infer<typeof posteoSchema>;
