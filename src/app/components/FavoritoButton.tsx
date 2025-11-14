e43572e9dde4106ee5f1546628bcbeb52b23a053
Paúl Pichón <paul10_barca@hotmail.com>
2025-11-13 22:37:15 -0600

    refactor: se agregan estilos par amostrar el button de formas diferentes

--- src/app/components/FavoritoButton.tsx
+++ src/app/components/FavoritoButton.tsx
@@ -1,35 +1,48 @@
 "use client";
 
 import { FavoritoButtonProps } from "@/types/types";
-import perfil from "../ui/perfil/perfil.module.css";
 import Spinner from "./spinner";
 import { useFavorito } from "@/context/FavoritoContext";
+import { FiHeart } from "react-icons/fi";
 
 const FavoritoButton: React.FC<FavoritoButtonProps> = ({
   posteoId,
   autorId,
   initialFavorito,
   className,
+  onRemoved,
+  iconOnly = false,
 }) => {
   const { favoritosMap, loadingMap, toggleFavorito } = useFavorito();
 
   const esFavorito = favoritosMap[posteoId] ?? initialFavorito;
   const loading = loadingMap[posteoId] ?? false;
 
-  const handleClick = () => {
-    toggleFavorito(posteoId, autorId, initialFavorito);
+  const handleClick = async () => {
+    await toggleFavorito(posteoId, autorId, initialFavorito);
+    const ahoraEsFavorito = favoritosMap[posteoId] ?? !initialFavorito;
+    if (!ahoraEsFavorito && onRemoved) {
+      onRemoved(posteoId);
+    }
   };
 
   return (
     <button
       type="button"
       disabled={loading}
-      className={className ?? perfil.btn_opciones_publicaciones}
       onClick={handleClick}
-      style={{ minWidth: "140px" }}
+      className={className}
+      title={esFavorito ? "Quitar de favoritos" : "Añadir a favoritos"}
     >
       {loading ? (
-        <Spinner size="20px" />
+        <Spinner size="18px" />
+      ) : iconOnly ? (
+        <FiHeart 
+          size={20}
+          strokeWidth={2.2}
+          color={esFavorito ? "red" : "red"}
+          fill={esFavorito ? "red" : "red"} // 👈 sólido si es favorito
+        />
       ) : esFavorito ? (
         "Quitar de favoritos"
       ) : (
