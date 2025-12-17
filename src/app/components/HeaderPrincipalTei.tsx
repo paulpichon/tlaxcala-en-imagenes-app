74b491a07be1d3a925fbc755fb0e71de7b0f10dd
Paúl Pichón <paul10_barca@hotmail.com>
2025-12-17 12:57:17 -0600

    refactor: titulo principal en mayusculas

--- src/app/components/HeaderPrincipalTei.tsx
+++ src/app/components/HeaderPrincipalTei.tsx
@@ -3,7 +3,7 @@ export function HeaderPrincipalTei() {
     return (
         <div className="col-md-12">
             <div className="contenedor_tei_index">
-                <h1 className="titulo_principal_h1">TlaxApp</h1>
+                <h1 className="titulo_principal_h1 text-uppercase">TLAXAPP</h1>
 			    <h6 className="subtitulo_h6">Tlaxcala, en una sola app.</h6>
             </div>
 		</div>
