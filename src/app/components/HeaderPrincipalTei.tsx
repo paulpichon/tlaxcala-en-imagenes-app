90ca740df391b782b072a89ac25bd1cb9461fa69
Paúl Pichón <paul10_barca@hotmail.com>
2026-01-23 17:36:49 -0600

    styles: se agrega el icono de TlaxApp

--- src/app/components/HeaderPrincipalTei.tsx
+++ src/app/components/HeaderPrincipalTei.tsx
@@ -1,11 +1,22 @@
 // Componente header de index, cuentas, crear-cuenta, login de TLAXCALA EN IMAGENES por TAD
+import Image from "next/image";
+
+import iconoTlaxapp from "@/../public/assets/icono-tlaxapp-beige.png";
+
 export function HeaderPrincipalTei() {
-    return (
-        <div className="col-md-12">
-            <div className="contenedor_tei_index">
-                <h1 className="titulo_principal_h1 text-uppercase">TLAXAPP</h1>
-			    <h6 className="subtitulo_h6">Tlaxcala, en una sola app.</h6>
-            </div>
-		</div>
-    );
+  return (
+    <div className="col-md-12 text-center mb-4">
+      <Image
+        src={iconoTlaxapp}
+        alt="TlaxApp"
+        width={85}
+        height={85}
+        priority
+        style={{ marginBottom: "0.75rem" }}
+      />
+
+      <h1 className="titulo_principal_h1">TlaxApp</h1>
+      <p className="subtitulo_h6">Tlaxcala, contada por su gente.</p>
+    </div>
+  );
 }
