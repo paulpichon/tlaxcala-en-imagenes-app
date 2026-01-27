4c35fe9fd6d4d8520d33d3ebc0c65da8e2309d99
Paúl Pichón <paul10_barca@hotmail.com>
2026-01-27 12:09:26 -0600

    styles: cambios de estilos en pagina y CSS

--- src/app/components/HeaderSuperior.tsx
+++ src/app/components/HeaderSuperior.tsx
@@ -1,13 +1,23 @@
-// Header superior de las páginas principales de la APP: inicio, perfil, notificaciones, etc etc
 'use client';
+
+import Image from "next/image";
+import Link from "next/link";
+
 export default function HeaderSuperior() {
     return (
-        <>
-            <nav className="navbar-expand-lg menu_superior_central">
-                <h1 className="titulo_header_superior_app">
-                    TLAXAPP
-                </h1>
-            </nav>
-        </>
+        <nav className="menu_superior_central">
+            <Link href="/inicio" className="header_marca">
+                <Image
+                    src="/assets/icono-tlaxapp-beige.png"
+                    alt="TlaxApp"
+                    width={50}
+                    height={50}
+                    priority
+                />
+                <span className="titulo_header_superior_app">
+                    TlaxApp
+                </span>
+            </Link>
+        </nav>
     );
 }
