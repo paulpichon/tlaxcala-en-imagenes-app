6514d93b3d60d6bd28dfcca940c1e1d09fe6c631
Paúl Pichón <paul10_barca@hotmail.com>
2026-02-20 11:30:59 -0600

    refactor: se mejora el ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,25 +1,29 @@
 'use client';
 
 import { useAuth } from '@/context/AuthContext';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
-// Spinner de carga
 import Spinner from '@/app/components/spinner';
-// Bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const { user, loading } = useAuth();
   const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useEffect(() => {
-    // Solo redirige si ya terminó de cargar y no hay usuario
     if (!loading && !user) {
-      router.push('/cuentas/login');
-    }
-  }, [user, loading, router]);
+      // Construimos la URL completa actual
+      const fullPath =
+        pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '');
 
-  // Puedes mostrar una pantalla de carga mientras se verifica
+      router.replace(
+        `/cuentas/login?redirect=${encodeURIComponent(fullPath)}`
+      );
+    }
+  }, [user, loading, router, pathname, searchParams]);
+
   if (loading || !user) {
     return (
       <div className="d-flex justify-content-center align-items-center vh-100">
