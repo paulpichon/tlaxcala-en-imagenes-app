3acee9d3ce81a6075158ff2bd98520065b10f356
Paúl Pichón <paul10_barca@hotmail.com>
2026-07-24 20:38:38 -0600

    Se podifica robots.tsx

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: "*",
         disallow: [
           "/api/",
-          "/posteo/",
           "/configuracion/",
           "/notificaciones/",
         ],
