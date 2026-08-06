12766a5e1ee8727f3102b69e88cea12a2b680888
Paúl Pichón <paul10_barca@hotmail.com>
2026-08-05 19:47:48 -0600

    Refactor: se agregan variables de entorno para las imagenes de PUBLICIDAD

--- src/context/PublicidadContext.tsx
+++ src/context/PublicidadContext.tsx
@@ -5,19 +5,19 @@ import { createContext, useContext, useState, useEffect, useCallback, useRef } f
 const anuncios = [
     {
       id: 1,
-      imagen: "https://res.cloudinary.com/dy9prn3ue/image/upload/v1764043828/438274959_292303987281616_1974015234189660361_n_nofvng.jpg",
+      imagen: `${process.env.NEXT_PUBLIC_IMAGEN_PUBLICIDAD_UNO}`,
       // titulo: "Explora las fotos mejor votadas del mes",
       url: "https://linktr.ee/olivarestaurantetlax?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGna3nkdD9iJMzC-Sd_mzqe6jp2zKhCXhbe8m9MRRf6K6X4lPfdWJTdhDN8rmU_aem_Oyx2DT5GsY8eL37XYXDMYg"
     },
     {
       id: 2,
-      imagen: "https://res.cloudinary.com/dy9prn3ue/image/upload/v1764044572/LO-blanco_r0oesk.png",
+      imagen: `${process.env.NEXT_PUBLIC_IMAGEN_PUBLICIDAD_DOS}`,
       // titulo: "Descubre los lugares más visitados",
       url: "https://lenayolivo.com.mx"
     },
     {
       id: 3,
-      imagen: "https://res.cloudinary.com/dy9prn3ue/image/upload/v1764044508/542769250_18047544959634987_7908904251530447003_n_qazjjm.jpg",
+      imagen: `${process.env.NEXT_PUBLIC_IMAGEN_PUBLICIDAD_TRES}`,
       // titulo: "Participa subiendo tu mejor foto",
       url: "https://www.instagram.com/brindisi_panaderiaartesanal/"
     }
